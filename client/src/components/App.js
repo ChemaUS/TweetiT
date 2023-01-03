83c4968ab5d8fb0befc4cbcc0cda731f6a6fed9d
Cesar Hernandez <chema27281@gmail.com>
2023-01-03 16:50:59 -0500

    login and signup routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-
+import { Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Login from './Login'
 import Signup from './Signup'
 import Profile from './Profile'
@@ -28,8 +29,27 @@ function App() {
 
   return (
     <div className="App">
+
+      <button>
+        <Link className="" to="/Login">
+          Login
+        </Link>
+      </button>
+      <button>
+        <Link className="" to="/Signup">
+          Signup
+        </Link>
+      </button>
+
+      <Switch>
+        <Route path="/Login">
       <Login user={user} setUser={setUser} />
+        </Route>
+
+        <Route path="/Signup">
       <Signup user={user} setUser={setUser} />
+        </Route>
+      </Switch>
       <Profile user={user} />
       <button onClick={handleLogout}>Logout</button>
 
