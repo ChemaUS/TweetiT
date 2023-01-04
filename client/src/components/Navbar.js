9ba9b72cece72c38df66cbfe0dd4421522eaf62a
Bernard Legaspi <blegaspi26@gmail.com>
2023-01-04 15:24:04 -0500

    added icons to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { HiHome } from "react-icons/hi";
+import { HiBell } from "react-icons/hi";
+import { HiMail } from "react-icons/hi";
+import { HiBookmark } from "react-icons/hi";
+import { HiUser } from "react-icons/hi";
+
 
 function Navbar() {
 
@@ -6,28 +12,33 @@ function Navbar() {
     return (
         <div className="navbar">
             <div className="home">
-                <OutlineHome />
+                <HiHome />
                 <p>Home</p>
             </div>
 
             <div className="notifications">
+                <HiBell />
                 <p>Notifications</p>
             </div>
 
             <div className="messages">
+                <HiMail />
                 <p>Messages</p>
             </div>
 
             <div className="bookmarks">
+                <HiBookmark />
                 <p>Bookmarks</p>
             </div>
 
             <div className="profile">
+                <HiUser />
                 <p>Profile</p>
             </div>
 
-        </div>
+        </div >
     )
 }
+// add onClick and route for Profile icon to lead to profile file
 
 export default Navbar; 
